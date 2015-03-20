bd6d63163f4ced057ee0eeaaaaed8dea7734259f
rogers5551 <rogers5551@gmail.com>
2015-03-20 16:58:58 -0400

    initial commit

---
+++ atm/features/step_definitions/steps.rb
@@ -0,0 +1,35 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'nice_bank')
+
+CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
+  digits.to_i
+end
+
+module KnowsTheDomain
+  def my_account
+    @my_account ||= Account.new
+  end
+  
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+  
+  def teller
+    @teller ||= Teller.new(cash_slot)
+  end
+end
+
+World(KnowsTheDomain)
+
+Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
+  my_account.deposit(amount)
+  my_account.balance.should eq(amount), 
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
+
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  teller.withdraw_from(my_account, amount)
+end
+
+Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
+  cash_slot.contents.should == amount
+end
